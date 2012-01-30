617c654fba3c831c7a414a74935c2cea8d636574
Nypias <nypias@gmail.com>
2012-01-30 13:52:24 +0100

    Ajout de la longueur du message a envoyer en int

--- socketGHome.py
+++ socketGHome.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 class SocketGHome :
     '''
@@ -6,15 +7,16 @@ class SocketGHome :
     '''
     
     def __init__(self) :
-        self.hote = "134.214.167.14"
-        self.port = 4433
+        self.hote = "127.0.0.1"
+        self.port = 8080
         self.connexionServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connexionServer.connect((self.hote, self.port))
         print("Connexion etablie avec le serveur sur le port {}".format(self.port))
     
     def sendMsg (self, message):
         # On envoie la taille du message en octets dans un premier temps
-        # self.connexionServer.send(len(message))
+        print str(len(message))
+        self.connexionServer.send(struct.pack('!i',len(message)))
         
         # On envoie le message ensuite    
         message = message.encode()
