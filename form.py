b8f4d694d5cff6599f205fb2df2f2d6216e77029
Thomas FATTAL <thomas.fattal@insa-lyon.fr>
2012-02-03 01:41:25 +0100

    Ajout de la classe Transport + Hack pour modifier le JSON du message + Ajout de la suppression d'une regle. Parsing OK. TODO suppression

--- form.py
+++ form.py
@@ -1,18 +1,20 @@
 from twisted.web import resource
 import simplejson as json
 from rule import Rule
-from transport import TransportGHome
 
 class Form (resource.Resource):
     
-    def __init__(self,ensRules):
+    def __init__(self,ensRules, transport):
         self.ensembleRules = ensRules
-        #self.transport = TransportGHome()
+        self.transport = transport
     
     def render_GET(self, request):
         '''
         Methode de reponse pour localhost:8000/form
         '''
+        
+        print "recu requete"
+        
         # On recupere le json de la requete POST
         datapost = request.content.getvalue()
         data = json.loads(datapost)
@@ -31,10 +33,23 @@ class Form (resource.Resource):
         '''
         
         if (data["type"] == "newRule"):
+            # Nouvelle regle donnee par le client ; A envoyer au serveur GHome
             rule = Rule(data["rule"])     
+            rule.priority = "1"
+            rule.name = "Nouvelle"
             self.ensembleRules.ajouterRule(rule)
             jsonMsg = rule.createJsonRule()
+            
+            jsonMsg = jsonMsg.replace('"type": u', '"type": ');
+            jsonMsg = jsonMsg.replace('"rightOp": u', '"rightOp": ');
+            jsonMsg = jsonMsg.replace('"leftOp": u', '"leftOp": ');
+            jsonMsg = jsonMsg.replace('"actuator": u', '"actuator": ');
+            jsonMsg = jsonMsg.replace('"value": u', '"value": ');
+            
+            print jsonMsg
             self.transport.sendRule(jsonMsg)
+            
         elif (data["type"] == "supprRule"):
+            # Suppression d'une regle
             nomRule = data["ruleName"]
             self.ensembleRules.supprimerRule(nomRule)
