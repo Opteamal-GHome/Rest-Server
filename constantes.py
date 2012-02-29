f0df343e4af5f0796221a88c06e26e16be46f7df
Thomas FATTAL <thomas.fattal@insa-lyon.fr>
2012-02-29 23:15:45 +0100

    Chmod sur les fichiers

--- constantes.py
+++ constantes.py
@@ -1,9 +1,9 @@
-ipServeurGHome = "134.214.167.113"
-#ipServeurGHome = "134.214.167.213"
+#ipServeurGHome = "134.214.167.113"
+ipServeurGHome = "134.214.167.213"
 #ipServeurGHome = "127.0.0.1"      # localhost
 portServeurGHome = 80
 
-portServerData = 421
+portServerData = 2000
 
 portServeurWeb = 8080
 
